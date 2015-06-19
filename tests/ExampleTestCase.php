adc52093e5263502a9bc9261edd784950d595e03
Maxime Fabre <ehtnam6@gmail.com>
2015-06-19 15:17:44 +0200

    Make TestCase abstract

--- tests/ExampleTestCase.php
+++ tests/ExampleTestCase.php
@@ -3,7 +3,7 @@ namespace Arrounded\Example;
 
 use PHPUnit_Framework_TestCase;
 
-class ExampleTestCase extends PHPUnit_Framework_TestCase
+abstract class ExampleTestCase extends PHPUnit_Framework_TestCase
 {
     // ...
 }
